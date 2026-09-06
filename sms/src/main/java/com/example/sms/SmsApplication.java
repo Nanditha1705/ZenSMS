2c2ddc214f7169d4dfbe76c0d7daf9cc6af5b67c
Nanditha1705 <nandithapeddineni@gmail.com>
2026-09-06 19:58:57 +0530

    Add prog congig for Render deployment

--- sms/src/main/java/com/example/sms/SmsApplication.java
+++ sms/src/main/java/com/example/sms/SmsApplication.java
@@ -7,8 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmsApplication {
 
 	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(SmsApplication.class);
+        String dbUrl = System.getenv("DATABASE_URL");
+        if (dbUrl != null && !dbUrl.isEmpty()) {
+            app.setAdditionalProfiles("prod");
+        }
+        app.run(args);
 		
-		SpringApplication.run(SmsApplication.class, args);
 	}
 	
 }
